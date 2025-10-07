dc29bf1d12d7827439a28377fdfe38013038abf9
Keerthashanthini <151423400+Keerthashanthini@users.noreply.github.com>
2025-10-07 15:09:58 +0530

    Create Factors_Of_num.java

---
+++ Factors_Of_num.java
@@ -0,0 +1,14 @@
+// Online Java Compiler
+// Use this editor to write, compile and run your Java code online
+import java.util.*;
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        for(int i=1;i<n;i++){
+            if(n%i==0){
+                System.out.print(i+" ");
+            }
+        }
+    }
+}
